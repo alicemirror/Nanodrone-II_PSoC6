c188050f788979f10cb1cc1b133634a1848d97eb
Jan Cumps <jcumps@gmail.com>
2021-03-20 12:49:19 +0100

    include UART and telemetry tasks and configuration adapted binary name and optimizer settings in makefile

---
+++ source/telemetry_queue.h
@@ -0,0 +1,16 @@
+/*
+ * telemetry_queue.h
+ *
+ *  Created on: 13 mrt. 2021
+ *      Author: jancu
+ */
+
+#ifndef TELEMETRY_QUEUE_H_
+#define TELEMETRY_QUEUE_H_
+
+
+#define TELEMETRY_MESSAGE_SIZE 16
+
+void initTelemetryQueue();
+
+#endif /* TELEMETRY_QUEUE_H_ */
